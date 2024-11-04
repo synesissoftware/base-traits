42044ae56ab71a33254ae78c6915fe554c00976e
Matt Wilson <matthew.wilson.synesis@gmail.com>
2024-11-04 13:46:04 +1100

    ~ ordering

--- src/lib.rs
+++ src/lib.rs
@@ -18,14 +18,14 @@ mod traits;
 
 pub use traits::{
     AsF64,
+    AsI128,
     AsI32,
     AsI64,
-    AsI128,
     AsISize,
     AsStr,
+    AsU128,
     AsU32,
     AsU64,
-    AsU128,
     AsUSize,
     Infinity,
     IsDefault,
